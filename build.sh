ef09343206f24d09e8fc671e326147f877a6f08c
ybellod <165201622+ybellod@users.noreply.github.com>
2024-09-28 12:50:03 -0400

    createsuperadmin

--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py poblar_instancias
+# Crear superusuario automáticamente si no existe
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser(username='admin', email='admin@example.com', password='adminpass')"
